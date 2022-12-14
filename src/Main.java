ebc23100a76c64e52a5b9b66925e3c54ebe0283b
faxcon08 <faxcon-08@mail.ru>
2022-12-14 19:06:43 +0300

    add new Branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+
+    public static void printMessage(String str) {
+        System.out.println(ANSI_RED+str+ANSI_RESET);
+    }
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
     }
 }
